c5476e9150326d063d0485b34b2b28aa4183b2a9
Jayanta Adhikary <adhikaryjayanta@outlook.com>
2023-07-23 18:14:07 +0530

    Added navigation to the restaurants

--- StartingProject/LocationsView.swift
+++ StartingProject/LocationsView.swift
@@ -7,14 +7,20 @@ struct LocationsView: View {
             LittleLemonLogo()
                 .padding(.top, 50)
             
-            EmptyView() // 2
+            Text(model.displayingReservationForm ? "Reservation Details" : "Select a location" )
             .padding([.leading, .trailing], 40)
             .padding([.top, .bottom], 8)
             .background(Color.gray.opacity(0.2))
             .cornerRadius(20)
             
             NavigationView {
-                EmptyView() // 3
+                List{
+                    ForEach(model.restaurants, id:\.self){ restaurant in
+                        NavigationLink(destination: ReservationForm(restaurant)){
+                            RestaurantView(restaurant)
+                        }
+                    }
+                }
                 .navigationBarTitle("")
                 .navigationBarHidden(true)
             }
